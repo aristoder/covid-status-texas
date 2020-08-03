ff95522fcc53f0f2889583587efc471e27c2a4ae
Aristroder <aristroder@gmail.com>
2020-08-03 12:46:25 -0500

    Move the module installation check from being checked every time to if the program fails with exception ModuleNotFoundError

--- covid.py
+++ covid.py
@@ -540,95 +540,95 @@ def installThisModule(modulename):
     import os
     os.system("pip3 install " + modulename + " > /dev/null")
 
-# check installation of getkey
-try:
-    import getkey
-except ModuleNotFoundError:
-    response=input("Module getkey not found.\nDo you want to install getkey? (Internet connection required).")
-    if responseYes(response):
-        installThisModule("getkey")
+if __name__ == "__main__":
+    try:
+        clear()
+        # accessing internet to update the local files
+        t = input("Do you want to update the database from the server? ")
+        if responseYes(t):
+            update_db(general_data_link, general_data_filename)
+            update_db(case_data_county_link, case_data_county_filename)
+            update_db(fat_data_county_link, fat_data_county_filename)
+            update_db(cumulative_tests_data_county_link, cumulative_tests_data_county_filename)
+        # reading files into memory
+        clear()
+        print("Reading files....")
+        xceldata_texas = read_db(general_data_filename, format=0)
+        xceldata_cumulative_case_county = read_db(case_data_county_filename, format=1)
+        xceldata_cumulative_fat_county = read_db(fat_data_county_filename, format=1)
+        xceldata_cumulative_tests_county = read_db(cumulative_tests_data_county_filename, format=1)
+        # decoding and menu calling
+        data_texas = data_decode(xceldata_texas)
+        del(xceldata_texas)
+        menu(data_texas, xceldata_cumulative_case_county, xceldata_cumulative_fat_county, xceldata_cumulative_tests_county)
+        clear()
+        pass
+    except ModuleNotFoundError:
+        # check installation of getkey
         try:
             import getkey
         except ModuleNotFoundError:
-            print("Module installation failed. Please run this command \"pip3 (or pip) install getkey\" and try fixing manually.")
-            input()
-            quit()
-        else:
-            print("Installation successful.")
-    else:
-        quit()
-# check installation of xlrd
-try:
-    import xlrd
-except ModuleNotFoundError:
-    response=input("Module xlrd not found.\nDo you want to install xlrd? (Internet connection required).")
-    if responseYes(response):
-        installThisModule("xlrd")
+            response=input("Module getkey not found.\nDo you want to install getkey? (Internet connection required).")
+            if responseYes(response):
+                installThisModule("getkey")
+                try:
+                    import getkey
+                except ModuleNotFoundError:
+                    print("Module installation failed. Please run this command \"pip3 (or pip) install getkey\" and try fixing manually.")
+                    input()
+                    quit()
+                else:
+                    print("Installation successful.")
+            else:
+                quit()
+        # check installation of xlrd
         try:
             import xlrd
         except ModuleNotFoundError:
-            print("Module installation failed. Please run this command \"pip3 (or pip) install xlrd\" and try fixing manually.")
-            input()
-            quit()
-        else:
-            print("Installation successful.")
-    else:
-        quit()
-# check installation of pandas
-try:
-    import pandas
-except ModuleNotFoundError:
-    response=input("Module pandas not found.\nDo you want to install pandas? (Internet connection required).")
-    if responseYes(response):
-        installThisModule("pandas")
+            response=input("Module xlrd not found.\nDo you want to install xlrd? (Internet connection required).")
+            if responseYes(response):
+                installThisModule("xlrd")
+                try:
+                    import xlrd
+                except ModuleNotFoundError:
+                    print("Module installation failed. Please run this command \"pip3 (or pip) install xlrd\" and try fixing manually.")
+                    input()
+                    quit()
+                else:
+                    print("Installation successful.")
+            else:
+                quit()
+        # check installation of pandas
         try:
             import pandas
         except ModuleNotFoundError:
-            print("Module installation failed. Please run this command \"pip3 (or pip) install pandas\" and try fixing manually.")
-            input()
-            quit()
-        else:
-            print("Installation successful.")
-    else:
-        quit()
-# check installation of matplotlib
-try:
-    import matplotlib
-except ModuleNotFoundError:
-    response=input("Module matplotlib not found.\nDo you want to install matplotlib? (Internet connection required).")
-    if responseYes(response):
-        installThisModule("matplotlib")
+            response=input("Module pandas not found.\nDo you want to install pandas? (Internet connection required).")
+            if responseYes(response):
+                installThisModule("pandas")
+                try:
+                    import pandas
+                except ModuleNotFoundError:
+                    print("Module installation failed. Please run this command \"pip3 (or pip) install pandas\" and try fixing manually.")
+                    input()
+                    quit()
+                else:
+                    print("Installation successful.")
+            else:
+                quit()
+        # check installation of matplotlib
         try:
             import matplotlib
         except ModuleNotFoundError:
-            print("Module installation failed. Please run this command \"pip3 (or pip) install matplotlib\" and try fixing manually.")
-            input()
-            quit()
-        else:
-            print("Installation successful.")
-    else:
-        quit()
-
-
-if __name__ == "__main__":
-    clear()
-    # accessing internet to update the local files
-    t = input("Do you want to update the database from the server? ")
-    if responseYes(t):
-        update_db(general_data_link, general_data_filename)
-        update_db(case_data_county_link, case_data_county_filename)
-        update_db(fat_data_county_link, fat_data_county_filename)
-        update_db(cumulative_tests_data_county_link, cumulative_tests_data_county_filename)
-    # reading files into memory
-    clear()
-    print("Reading files....")
-    xceldata_texas = read_db(general_data_filename, format=0)
-    xceldata_cumulative_case_county = read_db(case_data_county_filename, format=1)
-    xceldata_cumulative_fat_county = read_db(fat_data_county_filename, format=1)
-    xceldata_cumulative_tests_county = read_db(cumulative_tests_data_county_filename, format=1)
-    # decoding and menu calling
-    data_texas = data_decode(xceldata_texas)
-    del(xceldata_texas)
-    menu(data_texas, xceldata_cumulative_case_county, xceldata_cumulative_fat_county, xceldata_cumulative_tests_county)
-    clear()
-    pass
+            response=input("Module matplotlib not found.\nDo you want to install matplotlib? (Internet connection required).")
+            if responseYes(response):
+                installThisModule("matplotlib")
+                try:
+                    import matplotlib
+                except ModuleNotFoundError:
+                    print("Module installation failed. Please run this command \"pip3 (or pip) install matplotlib\" and try fixing manually.")
+                    input()
+                    quit()
+                else:
+                    print("Installation successful.")
+            else:
+                quit()
